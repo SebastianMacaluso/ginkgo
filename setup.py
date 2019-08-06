f933b06c299c34d6964486cdc5c2fa1e09c92452
dpappadopulo <dpappadopulo@bloomberg.net>
2019-08-06 15:22:31 -0400

    add clean-dist on Makefile

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ToyJetsShower",
-    version="0.2",
+    version="0.1",
     description="Toy Jets Shower Generator",
     url="https://github.com/SebastianMacaluso/ToyJetsShower",
     author="Sebastian Macaluso, Kyle Cranmer, Duccio Pappadopulo",
