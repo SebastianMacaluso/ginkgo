0674185311ec357e5eb7c6d44e73d6d4b81acec1
SebastianMacaluso <sm4511@nyu.edu>
2020-01-23 11:11:56 -0500

    updates

--- showerSim/likelihood.py
+++ showerSim/likelihood.py
@@ -43,6 +43,9 @@ def split_logLH(pL, delta_L, pR, delta_R, delta_min, lam):
         + np.log(1 / 2 / np.pi)
     )
 
+    if delta_P < delta_min:
+        logLH = - np.inf
+
     return logLH, p, delta_P, phi
 
 
@@ -103,6 +106,7 @@ def fill_jet_info(jet, parent_id=None):
     jet["deltas"] = deltas
     jet["draws"] = draws
 
+    return jet
 
 def _get_jet_info(jet, root_id=None, parent_id=None, deltas=None, draws=None):
     """
@@ -147,7 +151,7 @@ def enrich_jet_logLH(jet, Lambda=None, delta_min=None, dij=False, alpha = None):
     root_id = jet["root_id"]
 
     if Lambda is None:
-        Lambda = jet.get("Lambda")
+        Lambda = float(jet.get("Lambda"))
         if Lambda is None:
             raise ValueError(f"No Lambda specified by the jet.")
     if delta_min is None:
@@ -169,6 +173,7 @@ def enrich_jet_logLH(jet, Lambda=None, delta_min=None, dij=False, alpha = None):
     jet["logLH"] = np.asarray(logLH)
     jet["dij"] = dijList
 
+    return jet
 
 
 def _get_jet_logLH(
@@ -192,8 +197,14 @@ def _get_jet_logLH(
         idR = jet["tree"][root_id][1]
         pL = jet["content"][idL]
         pR = jet["content"][idR]
-        delta_L = jet["deltas"][idL]
-        delta_R = jet["deltas"][idR]
+        # delta_L = jet["deltas"][idL]
+        # delta_R = jet["deltas"][idR]
+
+
+        p_P =jet["content"][root_id]
+        delta_L = get_delta_PC(p_P, pL)
+        delta_R = get_delta_PC(p_P, pR)
+        # print(idL, idR,pL,pR,delta_L,delta_R,  delta_min, Lambda)
 
         # if M_Hard is not None and root_id == jet["root_id"]:
         #
@@ -204,11 +215,12 @@ def _get_jet_logLH(
 
         llh, _ , _ , _ = split_logLH(pL, delta_L, pR, delta_R, delta_min, Lambda)
         logLH.append(llh)
+        # print('logLH = ', llh)
 
         if dij:
 
             """ dij=min(pTi^(2 alpha),pTj^(2 alpha)) * [arccos((pi.pj)/|pi|*|pj|)]^2 """
-            # epsilon = 5e-7  # For numerical stability
+            # epsilon = 1e-6  # For numerical stability
             dijs= [float(llh)]
 
             for alpha in [-1,0,1]:
