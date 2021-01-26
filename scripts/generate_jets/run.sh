bb4c87cef5f75f71aac073a86b1e24fac4824f87
Sebastian Macaluso <sm4511@nyu.edu>
2021-01-26 20:29:18 -0300

    restructured ginkgo

--- scripts/generate_jets/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-python3 generate_jets.py \
-    --outdir "../../data" \
-    --filename "tree_$1_truth" \
-    --num_samples 1 \
-    --pt_cut 1 \
-#    --augmented_data
---
+++ src/ginkgo/simulator.py
@@ -0,0 +1,15 @@
+class Simulator:
+    """ Abstract simulator interface. """
+
+    def forward(self, inputs):
+        raise NotImplementedError()
+
+    def log_prob(self, inputs, outputs):
+        raise IntractableException
+
+    def __call__(self, inputs, **kwargs):
+        return self.forward(inputs, **kwargs)
+
+
+class IntractableException(Exception):
+    pass
