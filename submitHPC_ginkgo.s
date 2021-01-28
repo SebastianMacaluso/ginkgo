5f8e7b3e2ebece4bc8fd593596d516c38b60b64d
Sebastian Macaluso <sm4511@log-2.nyu.cluster>
2021-01-28 15:10:39 -0500

    Ginkgo HPC script

---
+++ submitHPC_ginkgo.s
@@ -0,0 +1,62 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=32GB
+#SBATCH --job-name=ginkgo
+#SBATCH --mail-type=END
+#SBATCH --mail-user=sm4511@nyu.edu
+#SBATCH --output=logs/slurm_%j.out
+
+module purge
+
+## executable
+##SRCDIR=$HOME/ReclusterTreeAlgorithms/scripts
+
+LOGSDIR=$SCRATCH/ginkgo/logs/
+mkdir -p $LOGSDIR
+
+RUNDIR=$SCRATCH/ginkgo/runs/run-${SLURM_JOB_ID/.*}
+mkdir -p $RUNDIR
+
+##cd $SLURM_SUBMIT_DIR
+##cp my_input_params.inp $RUNDIR
+
+##cd $RUNDIR
+##module load fftw/intel/3.3.5
+
+cd $SCRATCH
+pwd
+######  Ginkgo 2D  ############
+## W jets
+##python run2DShower.py --Nsamples=100 --id=${SLURM_ARRAY_TASK_ID} --jetType=Wjets
+
+##QCD jets
+#python run2DShower.py --Nsamples=100 --id=${SLURM_ARRAY_TASK_ID} --jetType=QCDjets
+
+##TELLIS
+#python run2DShower.py --Nsamples=100 --id=${SLURM_ARRAY_TASK_ID} --jetType=TrellisMw300
+
+#python run2DShower.py --Nsamples=100 --id=${SLURM_ARRAY_TASK_ID} --jetType=TrellisMw01
+
+#python run2DShower.py --Nsamples=100 --id=${SLURM_ARRAY_TASK_ID} --jetType=TrellisMw01B
+
+#To test
+#python run2DShower.py --Nsamples=3 --id=${SLURM_ARRAY_TASK_ID} --jetType=QCDjets
+
+
+singularity exec --overlay pytorch1.7.0-cuda11.0.ext3 /scratch/work/public/singularity/cuda11.0-cudnn8-devel-ubuntu18.04.sif /bin/bash
+source /ext3/env.sh
+
+cd $SCRATCH/ginkgo/src/ginkgo
+######  Ginkgo invariant mass  ############
+#python run_invMassGinkgo.py --Nsamples=100 --id=${SLURM_ARRAY_TASK_ID} --jetType=QCD
+python run_invMassGinkgo.py --jetType=QCD --Nsamples=10 --id=${SLURM_ARRAY_TASK_ID} --minLeaves=6 --maxLeaves=7 --maxNTry=20000
+
+## to submit(for 3 jobs): sbatch --array 0-2 submitHPC_ginkgo.s
+
+
+
+
+
